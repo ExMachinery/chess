b43dbaa40a43f730785ae15eca8061d250a930ee
ExMachinery <milkcarrotc@gmail.com>
2026-03-22 02:59:05 +0500

    Create method which returns allowed moves for rock (no castling)

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -2,7 +2,7 @@ require_relative 'board'
 require_relative 'game'
 
 class Chess_piece
-  attr_accessor :type, :colour, :position, :en_passant, :castiling
+  attr_accessor :type, :colour, :position, :en_passant, :castling
   def initialize(type, colour, position)
     types = [:pawn, :rock, :knight, :bishop, :queen, :king]
     @type = type
@@ -34,6 +34,43 @@ class Chess_piece
           elsif !board[row][column].nil? && board[row][column].colour != self.colour
             done = true
             moves << [row, column]
+          end
+        end
+      end
+    end
+    moves
+  end
+
+  def get_rock_moves(position, board)
+    moves = []
+    [1, -1].each do |i|
+      skip_row_direction, skip_column_direction = false, false
+      row, column = position[0], position[1]
+      until skip_column_direction && skip_row_direction
+        row = row + i if !skip_row_direction
+        column = column + i if !skip_column_direction
+        
+        if row < 0 || row > 7
+          skip_row_direction = true
+        else
+          moves << [row, position[1]] if board[row][position[1]] == nil
+          if !board[row][position[1]].nil? && board[row][position[1]].colour == self.colour
+            skip_row_direction = true
+          elsif !board[row][position[1]].nil? && board[row][position[1]].colour != self.colour
+            moves << [row, position[1]]
+            skip_row_direction = true
+          end
+        end
+
+        if column < 0 || column > 7
+          skip_column_direction = true
+        else
+          moves << [position[0], column] if board[position[0]][column] == nil
+          if !board[position[0]][column].nil? && board[position[0]][column].colour == self.colour
+            skip_column_direction = true
+          elsif !board[position[0]][column].nil? && board[position[0]][column].colour != self.colour
+            moves << [position[0], column]
+            skip_column_direction = true
           end
         end
       end
--- spec/chess_piece_spec.rb
+++ spec/chess_piece_spec.rb
@@ -2,12 +2,21 @@ require './lib/chess_piece'
 
 RSpec.describe Chess_piece do
   let(:board) { Array.new(8) {Array.new(8, nil)}}
-  let(:test) { Chess_piece.new(:bishop, :white, [3, 1])}
   describe "#get_bishop_moves" do
     it "returns correct moves" do
+      test = Chess_piece.new(:bishop, :white, [3, 1])
       board[3][1] = Chess_piece.new(:pawn, :white, [1, 3])
       board[3][5] = Chess_piece.new(:pawn, :black, [5, 3])
       expect(test.get_bishop_moves([1, 3], board)).to eql([[2, 4], [3, 5], [2, 2], [0, 4], [0, 2]])
+    end
+  end
+
+  describe "#get_rock_moves" do
+    it "returns correct moves" do
+      test = Chess_piece.new(:rock, :white, [1, 1])
+      board[3][1] = Chess_piece.new(:pawn, :black, [3, 1])
+      board[1][3] = Chess_piece.new(:pawn, :white, [1, 3])
+      expect(test.get_rock_moves([1, 1], board)).to eql([[2, 1], [1, 2], [3, 1], [0, 1], [1, 0]])
     end
   end
 end
