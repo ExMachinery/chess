25022760502aefb1e248fe7cc8c03ecf209132e8
ExMachinery <milkcarrotc@gmail.com>
2026-03-24 02:22:31 +0500

    Fix minor issues

--- lib/ui.rb
+++ lib/ui.rb
@@ -51,8 +51,8 @@ class UI
   end
 end
 
-# test = Board.new
-# test.prepare_for_new_game
-# ui = UI.new
-# ui.render_board(test.board, [[1, 0], [2, 0], [3, 0]])
+test = Board.new
+test.prepare_for_new_game
+ui = UI.new
+ui.render_board(test.board, [[2, 0], [3, 0]])
 # print "\e[48;2;255;127;0m #{piece} "
